482677d6c356630efdee4962da42846cf3089a0a
hleeminh <leeminh2221@gmail.com>
2023-03-28 19:33:09 +0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-      <main className="text-[50px]">
-        Home page...
+      <main className="text-[20px] text-green-600">
+        Home page... ok
       </main>
   )
 }
