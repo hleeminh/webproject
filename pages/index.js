77d491de17c8c60320a6ce33d2f7d3ab0f13ba3e
hleeminh <leeminh2221@gmail.com>
2023-04-07 12:12:07 +0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-      <main className="flex justify-center items-center m-auto text-[100px] text-black h-[800px]">
-        Chơi game === -1.100.000VNĐ cook
-      </main>
-  )
+  return <main className="">Base</main>;
 }
